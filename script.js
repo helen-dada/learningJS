40108caafb05beae202db72eec7d2d7b456a88cf
lenaS <e.a.semyonova.87@gmail.com>
2020-03-10 12:02:16 +0300

    Добавлены функции

--- script.js
+++ script.js
@@ -1,1 +1,50 @@
-alert(1);
+let money, time;
+
+function start() {
+  money = +prompt("Ваш бюджет на месяц?", '');
+  time = prompt("Введите дату в формате YYYY-MM-DD", '');
+
+  while (isNaN(money) || money == "" || money == null) {
+    money = +prompt("Ваш бюджет на месяц?", '');
+  }
+}
+start();
+
+
+let appData = {
+  budget: money,
+  expenses: {},
+  optionalExpenses: {},
+  income: [],
+  timeData: time,
+  savings: true
+};
+
+function chooseExpenses() {
+  for (let i = 0; i < 2; i++) {
+    let a = prompt("Введите обязательную статью расходов в этом месяце", ''),
+        b = +prompt("Во сколько это обойдётся?", '');    
+  if ( typeof(a) != null && typeof(b) != 0 && a != '' 
+       && b != '' && a.length < 50) {
+     console.log('done');
+     appData.expenses[a] = b;
+  } else {
+      i = i - 1;
+    }
+  }
+}
+chooseExpenses();
+
+appData.moneyPerDay = (appData.budget / 30).toFixed(1);
+
+alert("Ежедневный бюджет: " + appData.moneyPerDay);
+
+if (appData.moneyPerDay < 100) {
+  console.log("Низкий уровень дохода");
+} else if (appData.moneyPerDay >= 100 && appData.moneyPerDay < 2000) {
+  console.log("Средний уровень дохода");
+} else if (appData.moneyPerDay >= 2000) {
+  console.log("Средний уровень дохода");
+} else {
+  console.log("Произошла ошибка");
+};
